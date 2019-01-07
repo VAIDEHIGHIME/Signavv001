098cd194e83664a6a46ae126e48e0f87e3a74387
Vaidehi Ghime <vaidehighime@gmail.com>
2019-01-07 23:11:22 +0530

    Added SeeAndReactIssueActivity

---
+++ app/src/main/java/com/android/io/github/vaidehighime/www/signavv001/SeeAndReactIssueActivity.java
@@ -0,0 +1,13 @@
+package com.android.io.github.vaidehighime.www.signavv001;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SeeAndReactIssueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_and_react_issue);
+    }
+}
